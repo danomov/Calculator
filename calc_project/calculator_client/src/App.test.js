44c7280860b060923a22e147bb066b4c11f65853
danomov <dx555r@gmail.com>
2019-01-02 13:35:27 +0400

    Test Deletions

--- calc_project/calculator_client/src/App.test.js
+++ calc_project/calculator_client/src/App.test.js
@@ -1,28 +1,2 @@
-// import React from 'react';
-// import App from './App';
-// import { mount, shallow, render } from 'enzyme';
-
-// let component = mount(<App/>);
-
-// describe('looking for right h1', () => {
- 
-//   test('renders h1 correctly', () => {
-//     expect(component.find('h1').text()).toBe('Simple Calculator')
-//   });
-
-//   test('renders h1 with correct length', () => {
-//     expect(component.find('h1').length).toBe(1)
-//   });
-
-// });
-
-
-// describe('looking for main div', () => {
-  
-//   test('divs length', () => {
-//     expect(component.find('#App').length).toBe(11)
-//   });
-
-// });
 
 
--- calc_project/calculator_client/src/setupTests.js
+++ calc_project/calculator_client/src/setupTests.js
@@ -1,5 +1,1 @@
-import Adapter from 'enzyme-adapter-react-16';
-import Enzyme from 'enzyme';
-import 'jest-enzyme';
 
-Enzyme.configure({ adapter: new Adapter() });
